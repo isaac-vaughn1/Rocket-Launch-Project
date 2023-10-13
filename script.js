a6d74d191fecbdc2fb4c49ec0fb103e8479f1bea
Isaac Vaughn <isaac1vaughn@gmail.com>
2023-10-12 19:47:07 -0500

    configured data for display

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
+const images = document.getElementsByClassName('dataImg');
+const titles = document.getElementsByClassName('dataName');
+const describe = document.getElementsByClassName('dataDesc');
+const facts = document.getElementsByClassName('dataFact');
+
 const req = new XMLHttpRequest();
+
+window.addEventListener('load', function(){
+    getLaunchData();
+});
 
 function getLaunchData() {
     req.open("GET", "https://ll.thespacedevs.com/2.2.0/launch/upcoming/?is_crewed=true&limit=4");
     req.send();
 
-    req.onreadystatechange = getJSON;
+    req.onreadystatechange = function(){
+        if (req.readyState == 4 && req.status == 200) {
+            let res = req.responseText;
+    
+            let obj = JSON.parse(res);
+            displayLaunches(obj);
+        }
+    }; 
 }
 
 function getAstronautData() {
-    req.open("GET", "https://ll.thespacedevs.com/2.2.0/astronaut/?is_human=false&limit=4");
+    req.open("GET", "https://ll.thespacedevs.com/2.2.0/astronaut/?is_human=true&limit=4");
     req.send();
 
-    req.onreadystatechange = getJSON;
+    req.onreadystatechange = function(){
+        if (req.readyState == 4 && req.status == 200) {
+            let res = req.responseText;
+    
+            let obj = JSON.parse(res);
+            displayAstronauts(obj);
+        }
+    };
 }
 
-function getJSON() {
-    if (req.readyState == 4 && req.status == 200) {
-        var res = req.responseText;
+function displayAstronauts(obj) {
+    for (let i = 0; i < obj.results.length; i++) {
+        images[i].src = obj.results[i].profile_image;
+        titles[i].textContent = obj.results[i].name;
+        describe[i].textContent = obj.results[i].bio;
+        facts[i].textContent = `Spacewalks: ${obj.results[i].spacewalks_count}`;
+    }
+}
 
-        var obj = JSON.parse(res);
-        console.log(obj);
+function displayLaunches(obj){
+    for (let i = 0; i < obj.results.length; i++) {
+        images[i].src = obj.results[i].image;
+        titles[i].textContent = obj.results[i].name;
+        describe[i].textContent = obj.results[i].mission.description;
+        facts[i].textContent = `Launch Pad: ${obj.results[i].pad.name}`;
     }
 }
